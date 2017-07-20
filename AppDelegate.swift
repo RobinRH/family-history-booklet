2a96884bb04cfa6b0185e7e15ba53fd238157bc4
Robin <RobinRH@users.noreply.github.com>
2017-07-20 08:51:20 -0700

    Update AppDelegate.swift

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -1,9 +1,6 @@
 //
 //  AppDelegate.swift
-//  FamilyPhone
-//
-//  Created by Robin Reynolds on 6/27/15.
-//  Copyright (c) 2015 Robin Reynolds. All rights reserved.
+//  Copyright (c) 2017 Robin Reynolds. All rights reserved.
 //
 
 import UIKit
@@ -14,67 +11,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        FamilyTree.loadData()
+        GlobalData.readFamilyTree()
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
+    func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
-    func applicationDidEnterBackground(application: UIApplication) {
+    func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        FamilyTree.saveData()
+        GlobalData.writeFamilyTree()
     }
 
-    func applicationWillEnterForeground(application: UIApplication) {
+    func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-        FamilyTree.loadData()
+        GlobalData.readFamilyTree()
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
+    func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
-    func applicationWillTerminate(application: UIApplication) {
+    func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        FamilyTree.saveData()
+        GlobalData.writeFamilyTree()
     }
 
-/*
-    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
-        
-        
-        let fileManager = NSFileManager()
-        var paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentationDirectory, NSUserDomainMask, true)
-        let docDirectory = paths[0]
-        let inboxPath = docDirectory.stringByAppendingString("Inbox")
-        do {
-            var dirFiles = [String]()
-            try dirFiles = fileManager.contentsOfDirectoryAtPath(inboxPath)
-        }
-        catch {
-            // do something
-        }
 
-        
-//        NSFileManager *filemgr = [NSFileManager defaultManager];
-//        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-//        NSString *documentsDirectory = [paths objectAtIndex:0];
-//        NSString* inboxPath = [documentsDirectory stringByAppendingPathComponent:@"Inbox"];
-//        NSArray *dirFiles = [filemgr contentsOfDirectoryAtPath:inboxPath error:nil];
-    }
-    */
-    
-    
-    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
-        
-        let data = NSData(contentsOfURL: url)
-        FamilyTree.loadFamiliesWithData(data)
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        let data = try? Data(contentsOf: url)
+        GlobalData.readFamilyTree(json: data!)
         return true
     }
 
